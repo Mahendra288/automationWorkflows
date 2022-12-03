2fa27ed236ba692c5907214306c71979e9c59a75
BABA <baba.shaik@ibhubs.co.in>
2022-12-03 17:13:14 +0530

    minor changes

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module automationWorkflows
 go 1.19
 
 require (
+	github.com/agiledragon/gomonkey v2.0.2+incompatible
 	github.com/google/uuid v1.3.0
 	github.com/stretchr/testify v1.7.0
 )
--- tests/interactorTests/executeWorkflow_test.go
+++ tests/interactorTests/executeWorkflow_test.go
@@ -4,6 +4,7 @@ import (
 	"automationWorkflows/customErrors"
 	"automationWorkflows/enums"
 	"automationWorkflows/interactors"
+	"github.com/agiledragon/gomonkey"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 	"testing"
@@ -15,19 +16,20 @@ type executeWorkflowStorageInterfacesMock struct {
 
 func (o *executeWorkflowStorageInterfacesMock) GetWorkflowExecStatusFromRequestId(execReqUniqueId string) (string, error) {
 	args := o.Called(execReqUniqueId)
-	return enums.WorkFlowExecLogStatusEnum().Done, args.Error(1)
+	return args.Get(0).(string), args.Error(1)
 }
 
 func (o *executeWorkflowStorageInterfacesMock) GetLatestPublishedWorkflowExecConfigId(workflowId string) (string, error) {
 	args := o.Called(workflowId)
-	return "1223", args.Error(1)
+	return args.Get(0).(string), args.Error(1)
 }
 
-func TestAlreadyWorkflowExecutionConfigDone(t *testing.T) {
+func TestAlreadyWorkflowExecutionConfigIsInProgress(t *testing.T) {
 	// Arrange
 	execReqUniqueId := "12341"
+	workflowId := "workflow1"
 	reqDetails := interactors.WorkflowExecReqStruct{
-		WorkflowId: "1", SourceId: "Source1", WorkflowExecReqUniqueId: execReqUniqueId}
+		WorkflowId: workflowId, SourceId: "Source1", WorkflowExecReqUniqueId: execReqUniqueId}
 
 	// Mocking storage method
 	StorageInterfaceMockObj := new(executeWorkflowStorageInterfacesMock)
@@ -44,4 +46,54 @@ func TestAlreadyWorkflowExecutionConfigDone(t *testing.T) {
 
 	// Assert
 	assert.Equal(t, &customErrors.AlreadyExecutedWorkflowError{}, err)
+}
+
+func TestValidDetailsExecutesWorkflow(t *testing.T) {
+	// Arrange
+	execReqUniqueId := "12341"
+	workflowId := "workflow1"
+	latestExecConfigId := "LatestWorkflowExecutionConfigId1"
+
+	payload := map[string]string{}
+	triggerEventDetails := interactors.TriggerEventDetailsStruct{
+		EventEntity:   enums.TriggerEventEntityEnum().Lead,
+		EventEntityId: "1",
+		Payload:       payload,
+	}
+	reqDetails := interactors.WorkflowExecReqStruct{
+		WorkflowId: workflowId, SourceId: "Source1", WorkflowExecReqUniqueId: execReqUniqueId,
+		TriggerEventDetails: triggerEventDetails}
+	//
+	//initialLog := interactors.InitialWorkflowExecLogStruct{
+	//	ExecLogId:     uuid.NewString(),
+	//	ExecRequestId: reqDetails.WorkflowExecReqUniqueId,
+	//	SourceId:      reqDetails.SourceId,
+	//	SourceType:    reqDetails.SourceType,
+	//	ExecConfigId:  latestExecConfigId,
+	//	Payload:       string(payload),
+	//}
+	patches := gomonkey.ApplyFunc(
+		interactors.WorkflowExecLogInteractor{}.CreateInitialWorkflowExecLog, func(execLogStruct interactors.InitialWorkflowExecLogStruct) error {
+			return nil
+		})
+
+	defer patches.Reset()
+
+	// Mocking storage method
+	StorageInterfaceMockObj := new(executeWorkflowStorageInterfacesMock)
+	StorageInterfaceMockObj.On(
+		"GetWorkflowExecStatusFromRequestId", execReqUniqueId,
+	).Return("", nil)
+	StorageInterfaceMockObj.On(
+		"GetLatestPublishedWorkflowExecConfigId", workflowId,
+	).Return(latestExecConfigId, nil)
+	interactor := interactors.ExecuteWorkflowInteractor{
+		Storage: StorageInterfaceMockObj,
+	}
+
+	// Act
+	err := interactor.ExecuteWorkflow(reqDetails)
+
+	// Assert
+	assert.Equal(t, err, nil)
 }
